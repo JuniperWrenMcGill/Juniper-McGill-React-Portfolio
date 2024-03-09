d90ee1a0260524003ae3823a41e9443c252a9016
Juniper <juniperwrenarts@gmail.com>
2024-03-09 13:41:25 -0500

    debugging

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // main.js (or index.js)
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';''
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
